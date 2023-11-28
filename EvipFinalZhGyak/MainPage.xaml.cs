54eb96fee62c96df06df434c4283840e385fa2a8
morzsiHUN <morzsiHUN@gmail.com>
2023-11-28 01:36:17 +0100

    BindingContext átvíve XAML-be

--- EvipFinalZhGyak/MainPage.xaml.cs
+++ EvipFinalZhGyak/MainPage.xaml.cs
@@ -10,7 +10,7 @@ namespace EvipFinalZhGyak
         {
             dataModel = new DataModel();
             InitializeComponent();
-            BindingContext = dataModel;
+            //BindingContext = dataModel;
             dataModel.Cars.Add(new Car("asd","asd",21));
             dataModel.Cars.Add(new Car("asd2", "asd2", 212));
 
