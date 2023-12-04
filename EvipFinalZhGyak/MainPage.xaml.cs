950224c85f86b10002f8688e55bae01906937772
Zsombor <zsombor@sky-frames.com>
2023-12-04 20:25:43 +0100

    proba

--- EvipFinalZhGyak/MainPage.xaml.cs
+++ EvipFinalZhGyak/MainPage.xaml.cs
@@ -6,16 +6,22 @@ namespace EvipFinalZhGyak
     {
         public DataModel dataModel;
         public ICommand searchCommand { get; set; }
+        public ICommand MyCommand { get; }
+
 
         public MainPage()
         {
             dataModel = new DataModel();
+            MyCommand = new SearchCommand(dataModel,teszt);
             InitializeComponent();
             //BindingContext = dataModel;
+            BindingContext = this;
             dataModel.Cars.Add(new Car("asd","asd",21));
             dataModel.Cars.Add(new Car("asd2", "asd2", 212));
+        }
+        private void teszt()
+        {
 
-            searchCommand = new SearchCommand(dataModel);
         }
     }
 }
--- EvipFinalZhGyak/SearchCommand.cs
+++ EvipFinalZhGyak/SearchCommand.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace EvipFinalZhGyak
@@ -10,29 +6,46 @@ namespace EvipFinalZhGyak
     public class SearchCommand : ICommand
     {
         private DataModel dataModel;
-
-        public SearchCommand(DataModel dataModel)
-        {
-            this.dataModel = dataModel;
-        }
+        private Action _execute;
+        private Func<bool> _canExecute;
 
         public event EventHandler CanExecuteChanged;
 
+        public SearchCommand(DataModel dataModel, Action execute, Func<bool> canExecute = null)
+        {
+            this.dataModel = dataModel;
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
         public bool CanExecute(object parameter)
         {
+            // Az itt lévő feltétel lehet, hogy módosítást igényel attól függően, hogy mik a pontos követelmények.
+            // Például, ha az üres stringek elfogadhatóak, akkor ez a feltétel eltávolítható.
             if (dataModel.Brand == "" || dataModel.SelectedType == "")
             {
                 return false;
             }
-            return true;
+            return _canExecute == null || _canExecute();
         }
 
-        public async void Execute(object parameter)
+        public void Execute(object parameter)
         {
             if (CanExecute(parameter))
             {
-            dataModel.Search();
+                dataModel.Search();
+                _execute();  // Csak akkor hívjuk meg a _execute-t, ha a CanExecute igaz.
             }
+        }
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            OnCanExecuteChanged();
         }
     }
 }
