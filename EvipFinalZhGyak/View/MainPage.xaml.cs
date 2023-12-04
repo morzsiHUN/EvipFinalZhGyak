c2056b69f4aca64bfa99cf71bb7d8628f6e41ea5
morzsiHUN <morzsiHUN@gmail.com>
2023-12-04 22:13:21 +0100

    Mostmár működik a Command Binding

---
+++ EvipFinalZhGyak/View/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace EvipFinalZhGyak
+{
+    public partial class MainPage : ContentPage
+    {
+        public DataModel dataModel;
+        //public ICommand searchCommand { get; set; }
+        //public ICommand TestCommand { get; set; }
+
+        public readonly BindableProperty MyCommandProperty = 
+        BindableProperty.Create(nameof(MyCommand), typeof(ICommand), typeof(MainPage));
+
+        public ICommand MyCommand
+        {
+            get => (ICommand)GetValue(MyCommandProperty);
+            set => SetValue(MyCommandProperty, value);
+        }
+
+        public MainPage()
+        {
+            dataModel = new DataModel();
+            InitializeComponent();
+            //BindingContext = dataModel;
+            dataModel.Cars.Add(new Car("asd","asd",21));
+            dataModel.Cars.Add(new Car("asd2", "asd2", 212));
+
+            //TestCommand = new Command(() => SearchButton_Clicked());
+            MyCommand = new Command(SearchButton_Clicked);
+        }
+
+        public void SearchButton_Clicked()
+        {
+            System.Diagnostics.Debug.WriteLine("SearchButton_Clicked method is called");
+        }
+    }
+}
